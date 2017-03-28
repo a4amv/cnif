a375d10ce2c29e08563402c6038ad5bc263b1346
BarboraN <navratilova@cngroup.dk>
2017-03-28 12:55:06 +0200

    uprava nazvu sloupcu

--- JobFairInformationForm/src/JobFairInformationForm/Models/InformationForm/InformationFormViewModel.cs
+++ JobFairInformationForm/src/JobFairInformationForm/Models/InformationForm/InformationFormViewModel.cs
@@ -17,8 +17,9 @@ namespace JobFairInformationForm.Models.InformationForm
         /// Only output value
         /// </summary>
         //[Required(ErrorMessage = "Location is required.")]
+        [Display(Name ="Location")]
         public string Location { get; set; }
-
+        
         public List<Checkbox> LocationCheckboxes { get; set; } = new List<Checkbox>();
 
         /// <summary>
@@ -27,25 +28,31 @@ namespace JobFairInformationForm.Models.InformationForm
         public Dictionary<int, string> CheckedLocations { get; set; } = new Dictionary<int, string>();
 
         //[Required(ErrorMessage = "Preferred job is required.")]
+        [Display(Name ="Preferred job")]
         public string PreferredJob { get; set; }
 
         public string PreferredJobOther { get; set; }
 
+        [Display(Name = "Name")]
         [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
 
+        [Display(Name = "Surname")]
         [Required(ErrorMessage = "Surname is required.")]
         public string Surname { get; set; }
 
+        [Display(Name = "Phone number")]
         [Required(ErrorMessage = "Telephone is required.")]
-        [RegularExpression(@"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$", ErrorMessage = "Telephone number is not valid.")]
+        [RegularExpression(@"^\+?(\d[\d- ]+)?(\([\d- ]+\))?[\d- ]+\d$", ErrorMessage = "Telephone number is not valid.")]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Email")]
         [Required(ErrorMessage = "Email is required.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Display(Name = "Allocation")]
         [Required(ErrorMessage = "Allocation is required.")]
         [Range(1, 40)]
         public int Allocation { get; set; }
@@ -54,8 +61,10 @@ namespace JobFairInformationForm.Models.InformationForm
         [Display(Name = "Expected graduation date")]
         public DateTime GraduationDate { get; set; }
 
+        [Display(Name = "Education")]
         public string Education { get; set; }
 
+        [Display(Name = "Note")]
         public string NoteString { get; set; }
     }
 }
